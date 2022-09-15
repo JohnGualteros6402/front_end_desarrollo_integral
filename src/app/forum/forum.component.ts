e989a84a487742820c4784d640ba83e7ae8a70a4
Your Name <you@example.com>
2022-09-15 11:48:04 -0500

    forum

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -1,21 +1,34 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Question } from '../classes/question';
 import { QuestionService } from '../services/question.service';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
   styleUrls: ['./forum.component.css']
 })
-export class ForumComponent implements OnInit {
+export class ForumComponent implements OnInit, OnDestroy {
 
-  constructor( private router: Router, private http: HttpClient, private questionService: QuestionService) { }
+  constructor( private router: Router, private http: HttpClient, private questionService: QuestionService,  private route: ActivatedRoute) { }
+  id: number;
   email: any;
-  questions: Question = new Question();
+  questions:Question[];
+  question:any = null;
+  numberofquestions:number;
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject<any>();
+
   ngOnInit(): void {
     this.validateIsAuthenticated();
+    this.getQuestions();
+    this.getQuestionById();
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
   }
 
   validateIsAuthenticated(){
@@ -24,5 +37,21 @@ export class ForumComponent implements OnInit {
       return
     }
     return this.router.navigate(['login']);
+  }
+  getQuestionById(){
+    this.id = this.route.snapshot.params['id'];
+    this.questionService.getQuestionById(this.id).subscribe(data => {
+      this.question = data;
+    }, (error: any) => console.log(error));
+  }
+  private getQuestions(){
+    this.questionService.getListQuestions().subscribe(data => {
+      this.questions = data;
+      this.numberofquestions= this.questions.length;
+      this.dtTrigger.next(this.questions);
+    })
+  }
+  getQuestionAndRedirect(id: number){
+    this.router.navigate(['/forum', id]).then(res=>window.location.reload())
   }
 }
