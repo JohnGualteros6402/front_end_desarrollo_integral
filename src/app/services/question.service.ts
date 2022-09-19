ba1ac8ea3302aa8cc11daa680d82484683b8d42f
Andres Meza <andresmezad2519@gmail.com>
2022-09-19 10:50:21 -0500

    Foreign Key Create-Question

--- src/app/services/question.service.ts
+++ src/app/services/question.service.ts
@@ -4,27 +4,27 @@ import { Observable } from 'rxjs';
 import { Question } from '../classes/question';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuestionService {
-
-  private baseURL = "http://localhost:8081/api/v1/question";
+  private baseURL = 'http://localhost:8081/api/v1/question';
 
   constructor(private httpClient: HttpClient) {}
 
   getListQuestions(): Observable<Question[]> {
     return this.httpClient.get<Question[]>(`${this.baseURL}`);
   }
-  updateQuestion(id:number,question:Question) : Observable<Object>{
-    return this.httpClient.put(`${this.baseURL}/${id}`,question);
+  updateQuestion(id: number, question: Question): Observable<Object> {
+    return this.httpClient.put(`${this.baseURL}/${id}`, question);
   }
-  getQuestionById(id:number): Observable<Question>{
-    return this.httpClient.get<Question>(`${this.baseURL}/${id}`)
+  getQuestionById(id: number): Observable<Question> {
+    return this.httpClient.get<Question>(`${this.baseURL}/${id}`);
   }
   addQuestion(question: Question): Observable<Object> {
+    console.log(question);
     return this.httpClient.post(`${this.baseURL}`, question);
   }
-  deleteQuestion(id:number): Observable<Object>{
+  deleteQuestion(id: number): Observable<Object> {
     return this.httpClient.delete(`${this.baseURL}/${id}`);
   }
 }
